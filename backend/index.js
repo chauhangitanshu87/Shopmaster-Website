7ca5a71f6f4748ab0ad3307338a833de1ec5fae7
girishrana <ranagirish246@gmail.com>
2024-09-20 14:45:07 +0530

    3commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app =express();
+const mongoose = require('mongoose')
 
 const port = 3000
 
-app.get('/',(req,resp)=>{
-    resp.send('route working perfectly')
-})
+const connectDB= async ()=>{
+    mongoose.connect('mongodb+srv://gitanshuchauhan:gitanshu@cluster0.q0pht.mongodb.net/e-comm?retryWrites=true&w=majority&appName=Cluster0/');
+    const productSchema=new mongoose.Schema({});
+    const product = mongoose.model('product',productSchema)
+    const data =await product.find();
+    console.warn(data);
+}
+
+connectDB();
 
 app.listen(port, ()=>{
     console.log(`shopmaster backend listening at http:localhost:${port}`)
